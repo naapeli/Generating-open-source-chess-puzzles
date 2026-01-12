552919605c953d382ade978ddedc0e69b024bed2
Selkee Aatu <selkeea1@login4.triton.aalto.fi>
2026-01-12 17:01:50 +0200

    Supervised training working on triton gpu cluster with 1 gpu

--- src/supervised.sh
+++ src/supervised.sh
@@ -1,6 +1,9 @@
 #!/bin/bash -l
 #SBATCH --time=00:10:00
 #SBATCH --gpus=1
+#SBATCH --output=output.out
+#SBATCH --mem=32000
 
-module load scicomp-python-env
-python supervised_training.py
+module load mamba
+source activate environment
+srun python src/supervised_training.py
