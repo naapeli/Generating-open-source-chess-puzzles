779d7b9f1b914373b8640a6a026ff1eac8cbddde
Selkee Aatu <selkeea1@login4.triton.aalto.fi>
2026-01-14 17:10:38 +0200

    Shell script now for large scale runs

--- src/supervised.sh
+++ src/supervised.sh
@@ -1,14 +1,16 @@
 #!/bin/bash -l
-#SBATCH --time=00:10:00
+#SBATCH --job-name=MaskedDiffusion
+#SBATCH --time=24:00:00
 #SBATCH --output=output.out
-#SBATCH --mem=32G
-# #SBATCH --mem-per-gpu=32G
-# #SBATCH --ntasks=2
-# #SBATCH --gpus=2
-# #SBATCH --cpus-per-gpu=10
+#SBATCH --mem=64G
+#SBATCH --nodes=1
+#SBATCH --ntasks=4
+#SBATCH --gpus=h100:4
+#SBATCH --cpus-per-gpu=10
+#SBATCH --mail-user=aatu.selkee@aalto.fi
 
 
 module load mamba
 source activate environment
-# srun python src/supervised_training.py --distributed
-srun python src/supervised_training.py
+srun python src/supervised_training.py --distributed
+# srun python src/supervised_training.py
