ade0ac9abe6e7ef6c293007c0c4168afc964baa2
Selkee Aatu <aatu.selkee@gmail.com>
2026-02-02 15:11:06 +0200

    a few images

--- src/rl/espo.py
+++ src/rl/espo.py
@@ -4,84 +4,71 @@ import numpy as np
 import random
 
 
-n_quadrature = 10
+n_quadrature = 20  # 3
 t_points, quadrature_weights = np.polynomial.legendre.leggauss(n_quadrature)  # estimate the integral with a gaussian quadrature (https://arxiv.org/pdf/2510.08554)
 t_points, quadrature_weights = torch.from_numpy(t_points), torch.from_numpy(quadrature_weights)
 t_points, quadrature_weights = (1 - 0) / 2 * t_points + (1 + 0) / 2, (1 - 0) / 2 * quadrature_weights  # https://en.wikipedia.org/wiki/Gaussian_quadrature#Change_of_interval
 
 
 def kl_estimate(model_elbo, reference_elbo):  # k2 from http://joschu.net/blog/kl-approx.html and https://arxiv.org/pdf/2512.03759 (biased, but low variance estimator)
-    return 0.5 * torch.mean((model_elbo - reference_elbo) ** 2)
+    return 0.5 * torch.mean((model_elbo - reference_elbo) ** 2, dim=1)
 
-def espo_loss(model, reference_model, fens, themes, ratings, rewards, group_size, eps=0.2, beta=0.1):
-    n_samples, sequence_length = fens.shape  # fens.shape == (batch_size * group_size, sequence_length)  batch_size groups of size group_size
-    assert n_samples % group_size == 0
-    batch_size = n_samples // group_size
-
+def compute_elbo(model, fens, themes, ratings, mask=None, return_mask=False):
     device = ratings.device
-    config = model.config
+    n_samples = len(ratings)
 
-    # t = t_points[None, :].expand(batch_size * group_size, n_quadrature).reshape(-1).to(device)  # batch_size * group_size * n_quadrature
-    t = t_points.repeat(batch_size * group_size).to(device)  # batch_size * group_size * n_quadrature
-    # t = ((torch.rand(1) + torch.arange(n_samples) / n_samples) % 1).to(device)
-    alpha_t = config.masking_schedule(t)
+    t = t_points.repeat(n_samples).to(device)
+    alpha_t = model.config.masking_schedule(t)
     quadrature_weight = quadrature_weights.unsqueeze(0).to(device)
 
-    # (batch_size * group_size * n_quadrature, ...)
     fens = fens.repeat_interleave(n_quadrature, dim=0)
     themes = themes.repeat_interleave(n_quadrature, dim=0)
     ratings = ratings.repeat_interleave(n_quadrature, dim=0)
 
-    random_mask = torch.rand(fens.size(), device=device) < alpha_t.unsqueeze(1)
-    masked_fens = torch.where(random_mask, fens, config.mask_token)
+    random_mask = torch.rand(fens.size(), device=device) < alpha_t.unsqueeze(1) if mask is None else mask
+    masked_fens = torch.where(random_mask, fens, model.config.mask_token)
 
-    # use the same mask for both models (antithetic sampling from https://arxiv.org/pdf/2512.03759)
     logits = model(masked_fens, themes, ratings)
-    model_loss = model.elbo_loss(t, logits, fens, masked_fens)
-    with torch.no_grad():
-        logits = reference_model(masked_fens, themes, ratings)
-        reference_loss = reference_model.elbo_loss(t, logits, fens, masked_fens)
+    elbo = model.elbo_loss(t, logits, fens, masked_fens)
+    elbo = (quadrature_weight * elbo.reshape(n_samples, n_quadrature)).sum(dim=1)
+    if return_mask:
+        return elbo, random_mask
+    return elbo
 
-    # compute the integral and get the group structure back
-    model_loss = (quadrature_weight * model_loss.reshape(batch_size, group_size, n_quadrature)).sum(dim=2)
-    reference_loss = (quadrature_weight * reference_loss.reshape(batch_size, group_size, n_quadrature)).sum(dim=2)
-    rewards = rewards.reshape(batch_size, group_size)
-    rho = torch.exp(1 / sequence_length * (model_loss - reference_loss))
+def compute_elbo_basic(model, fens, themes, ratings, mask=None, return_mask=False):
+    device = ratings.device
+    n_samples = len(ratings)
 
-    # (batch_size,)
-    advantages = (rewards - rewards.mean(dim=1, keepdim=True)).to(device)  # Dr GRPO (do not normalize by the standard deviation) https://arxiv.org/pdf/2503.20783
-    loss = torch.minimum(rho * advantages, torch.clamp(rho, 1 - eps, 1 + eps) * advantages).mean(dim=1) - beta * kl_estimate(model_loss, reference_loss)  # (batch_size,)
-    return -loss  # maximize the loss above
+    t = ((torch.rand(1) + torch.arange(n_samples) / n_samples) % 1).to(device)
+    alpha_t = model.config.masking_schedule(t)
 
-def espo_loss_basic(model, reference_model, fens, themes, ratings, rewards, group_size, eps=0.2, beta=0.1):
+    random_mask = torch.rand(fens.size(), device=device) < alpha_t.unsqueeze(1) if mask is None else mask
+    masked_fens = torch.where(random_mask, fens, model.config.mask_token)
+
+    logits = model(masked_fens, themes, ratings)
+    elbo = model.elbo_loss(t, logits, fens, masked_fens)
+    if return_mask:
+        return elbo, random_mask
+    return elbo
+
+def espo_loss(model, reference_losses, old_losses, fens, themes, ratings, rewards, group_size, mask=None, eps=0.2, beta=0.1):
     n_samples, sequence_length = fens.shape  # fens.shape == (batch_size * group_size, sequence_length)  batch_size groups of size group_size
     assert n_samples % group_size == 0
     batch_size = n_samples // group_size
 
     device = ratings.device
-    config = model.config
+    model_loss = compute_elbo(model, fens, themes, ratings, mask=mask, return_mask=False)
+    # model_loss = compute_elbo_basic(model, fens, themes, ratings, mask=mask, return_mask=False)
 
-    t = ((torch.rand(1) + torch.arange(n_samples) / n_samples) % 1).to(device)
-    alpha_t = config.masking_schedule(t)
-
-    random_mask = torch.rand(fens.size(), device=device) < alpha_t.unsqueeze(1)
-    masked_fens = torch.where(random_mask, fens, config.mask_token)
-
-    # use the same mask for both models (antithetic sampling from https://arxiv.org/pdf/2512.03759)
-    logits = model(masked_fens, themes, ratings)
-    model_loss = model.elbo_loss(t, logits, fens, masked_fens)
-    with torch.no_grad():
-        logits = reference_model(masked_fens, themes, ratings)
-        reference_loss = reference_model.elbo_loss(t, logits, fens, masked_fens)
-
-    # get the group structure back
-    model_loss = model_loss.reshape(batch_size, group_size)
-    reference_loss = reference_loss.reshape(batch_size, group_size)
     rewards = rewards.reshape(batch_size, group_size)
+    model_loss = model_loss.reshape(batch_size, group_size)
+    reference_losses = reference_losses.reshape(batch_size, group_size)
+    old_losses = old_losses.reshape(batch_size, group_size)
+    rho = torch.exp(1 / sequence_length * (model_loss - reference_losses))  # importance sampling (generate from reference, update model)
 
-    advantages = (rewards - rewards.mean(dim=1, keepdim=True)).to(device)
-    rho = torch.exp(1 / sequence_length * (model_loss - reference_loss))
-    loss = torch.minimum(rho * advantages, torch.clamp(rho, 1 - eps, 1 + eps) * advantages).mean(dim=1) - beta * kl_estimate(model_loss, reference_loss)
+    # (batch_size,)
+    advantages = (rewards - rewards.mean(dim=1, keepdim=True)).to(device)  # Dr GRPO (do not normalize by the standard deviation) https://arxiv.org/pdf/2503.20783
+    loss = torch.minimum(rho * advantages, torch.clamp(rho, 1 - eps, 1 + eps) * advantages).mean(dim=1) - beta * kl_estimate(model_loss, old_losses)  # (batch_size,)
     return -loss  # maximize the loss above
 
 def generate_grouped_positions(model, themes, ratings, group_size, steps=256):
--- src/train_rl.py
+++ src/train_rl.py
@@ -10,7 +10,7 @@ import numpy as np
 from scipy.stats import gaussian_kde
 
 from MaskedDiffusion.model import MaskedDiffusion
-from rl.espo import espo_loss, espo_loss_basic, generate_grouped_positions, generate_random_themes
+from rl.espo import espo_loss, generate_grouped_positions, generate_random_themes, compute_elbo, compute_elbo_basic
 from tokenization.tokenization import theme_preprocessor, scale_ratings, tokens_to_fen
 from metrics.themes import legal, get_unique_puzzle_from_fen, counter_intuitive
 from metrics.cook import cook
@@ -27,18 +27,18 @@ model = MaskedDiffusion(config)
 model.load_state_dict(checkpoint["model"])
 model.to(device=device)
 
-reference_model = deepcopy(model)
-reference_model.to(device=device)
+old_model = deepcopy(model)
+old_model.to(device=device)
 
 n_gradient_updates_per_generation = 8  # https://arxiv.org/pdf/2512.03759 figure 5 (8 - 24 seems reasonable)
 total_steps = 20_000
-batch_size = 1
-group_size = 8  # 24  # 8 - 64 probably good?  # TODO: Look at the deep seek paper where GRPO was proposed (or the Dr GRPO paper)  in the overfitting task, 16 seems to be too low
+batch_size = 3
+group_size = 24  # 8 - 64 probably good?  # TODO: Look at the deep seek paper where GRPO was proposed (or the Dr GRPO paper)  in the overfitting task, 16 seems to be too low
 
 params_adam = [p for p in model.parameters() if p.ndim != 2]
 params_muon = [p for p in model.parameters() if p.ndim == 2]
-adam = AdamW(params_adam, lr=1e-2, weight_decay=0)  # 4e-3
-muon = Muon(params_muon, lr=1e-2, weight_decay=0)  # 4e-3
+adam = AdamW(params_adam, lr=4e-3, weight_decay=0)  # 4e-3
+muon = Muon(params_muon, lr=4e-3, weight_decay=0)  # 4e-3
 
 engine = SimpleEngine.popen_uci(base_path / ".." / "Stockfish" / "src" / "stockfish")
 # engine.configure({"Hash": 2048})
@@ -77,15 +77,21 @@ def get_reward(fen, themes, rating):
 
 
 step = 0
-# end = False
-end = True
+end = False
 while not end:
     themes, ratings = generate_random_themes(batch_size)
     themes_one_hot = torch.from_numpy(theme_preprocessor.transform(themes)).to(device=device, dtype=torch.float32)
     scaled_ratings = scale_ratings(ratings).to(device=device, dtype=torch.float32)
 
-    # generate the fens from the model
+    # generate the fens from the reference_model
+    reference_model = deepcopy(model)
+    reference_model.to(device)
     step_fens, step_themes, step_ratings = generate_grouped_positions(model, themes_one_hot, scaled_ratings, group_size, steps=128)  # 256
+
+    # precompute the elbos of the old model
+    with torch.no_grad():
+        reference_elbo, mask = compute_elbo(reference_model, step_fens, step_themes, step_ratings, return_mask=True)
+        old_elbo = compute_elbo(old_model, step_fens, step_themes, step_ratings, mask=mask)
 
     # compute the rewards
     rewards = torch.zeros(len(step_fens))
@@ -102,8 +108,9 @@ while not end:
         adam.zero_grad()
         muon.zero_grad()
 
-        loss = espo_loss(model, reference_model, step_fens, step_themes, step_ratings, rewards, group_size, eps=0.1, beta=0.1).mean()
+        loss = espo_loss(model, reference_elbo, old_elbo, step_fens, step_themes, step_ratings, rewards, group_size, mask, eps=0.1, beta=0.0).mean()
         total_loss += loss.item()
+        print(loss.item(), flush=True)
 
         loss.backward()
 
@@ -113,8 +120,6 @@ while not end:
         adam.step()
         muon.step()
 
-        # end = True
-
         if step >= total_steps:
             end = True
     
@@ -122,60 +127,73 @@ while not end:
 
 
 
-batch_size = 10
-group_size = 1
-themes, ratings = generate_random_themes(batch_size)
-themes_one_hot = torch.from_numpy(theme_preprocessor.transform(themes)).to(device=device, dtype=torch.float32)
-scaled_ratings = scale_ratings(ratings).to(device=device, dtype=torch.float32)
+# batch_size = 20
+# group_size = 1
+# themes, ratings = generate_random_themes(batch_size)
+# themes_one_hot = torch.from_numpy(theme_preprocessor.transform(themes)).to(device=device, dtype=torch.float32)
+# scaled_ratings = scale_ratings(ratings).to(device=device, dtype=torch.float32)
 
-step_fens, step_themes, step_ratings = generate_grouped_positions(model, themes_one_hot, scaled_ratings, group_size, steps=128)
+# step_fens, step_themes, step_ratings = generate_grouped_positions(model, themes_one_hot, scaled_ratings, group_size, steps=128)
 
-rewards = 10 * torch.rand(len(step_fens))
 
-losses = []
-basic_losses = []
-beta = 0
-eps = 0.1
-for substep in range(100):  # 1000
-    loss = espo_loss(model, reference_model, step_fens, step_themes, step_ratings, rewards, group_size, eps=eps, beta=beta)
-    losses.extend(loss.squeeze().tolist())
+
+# return elbo.reshape(n_samples, n_quadrature) to compute_elbo
+# from rl.espo import t_points
+
+# n_steps = 50
+# with torch.no_grad():
+#     elbos = compute_elbo(model, step_fens, step_themes, step_ratings).mean(dim=0)
+#     for substep in range(n_steps):
+#         elbos = elbos + compute_elbo(model, step_fens, step_themes, step_ratings).mean(dim=0)
+#     elbos = elbos / n_steps
+
+# plt.plot(t_points.numpy(), elbos.detach().cpu().numpy())
+# plt.savefig(base_path / "image3.png")
+
+
+
+# losses = []
+# basic_losses = []
+# for substep in range(10):  # 1000
+#     loss = compute_elbo(model, step_fens, step_themes, step_ratings)
+#     losses.extend(loss.squeeze().tolist())
     
-    loss = espo_loss_basic(model, reference_model, step_fens, step_themes, step_ratings, rewards, group_size, eps=eps, beta=beta)
-    basic_losses.extend(loss.squeeze().tolist())
+#     loss = compute_elbo_basic(model, step_fens, step_themes, step_ratings)
+#     basic_losses.extend(loss.squeeze().tolist())
 
-losses = np.array(losses)
-basic_losses = np.array(basic_losses)
-
-
-def plot_with_mode(data, label, color, x_limit=None):
-    kde = gaussian_kde(data)
-    x_limit = data.max() if x_limit is None else x_limit
-    x_range = np.linspace(0, x_limit, 1000)
-    pdf_values = kde(x_range)
-    mode_x = x_range[np.argmax(pdf_values)]
-    plt.plot(x_range, pdf_values, color=color, lw=2, label=f"{label} PDF")
-    plt.axvline(mode_x, color=color, linestyle='--', alpha=0.7, 
-                label=f"{label} Mode: {mode_x:.2f}")
-    plt.fill_between(x_range, pdf_values, alpha=0.2, color=color)
-    return mode_x
-
-plt.figure()
-plt.plot(np.cumsum(losses) / (np.arange(len(losses)) + 1), label="low variance")
-plt.plot(np.cumsum(basic_losses) / (np.arange(len(basic_losses)) + 1), label="high variance")
-plt.ylabel("ELBO loss")
-plt.legend()
-plt.savefig(base_path / "image2.png")
+# losses = np.array(losses)
+# basic_losses = np.array(basic_losses)
 
 
-plt.figure()
-try:
-    mode_low = plot_with_mode(losses, "Low Var (Quadrature)", "blue")
-    mode_high = plot_with_mode(basic_losses, "High Var (Basic)", "orange")
-except:
-    plt.hist(losses, alpha=0.5, density=True, label="Low Var (Quadrature)", color="blue")
-    plt.hist(basic_losses, alpha=0.5, density=True, label="High Var (Basic)", color="orange")
-plt.legend()
-plt.xlim((min(losses.min(), basic_losses.min()) - 2, max(np.quantile(losses, 0.95), np.quantile(basic_losses, 0.95)) + 2))
-plt.savefig(base_path / "image.png")
+# def plot_with_mode(data, label, color, x_limit=None):
+#     kde = gaussian_kde(data)
+#     x_limit = data.max() if x_limit is None else x_limit
+#     x_range = np.linspace(0, x_limit, 1000)
+#     pdf_values = kde(x_range)
+#     mode_x = x_range[np.argmax(pdf_values)]
+#     plt.plot(x_range, pdf_values, color=color, lw=2, label=f"{label} PDF")
+#     plt.axvline(mode_x, color=color, linestyle='--', alpha=0.7, 
+#                 label=f"{label} Mode: {mode_x:.2f}")
+#     plt.fill_between(x_range, pdf_values, alpha=0.2, color=color)
+#     return mode_x
+
+# plt.figure()
+# plt.plot(np.cumsum(losses) / (np.arange(len(losses)) + 1), label="low variance")
+# plt.plot(np.cumsum(basic_losses) / (np.arange(len(basic_losses)) + 1), label="high variance")
+# plt.ylabel("ELBO loss")
+# plt.legend()
+# plt.savefig(base_path / "image2.png")
+
+
+# plt.figure()
+# try:
+#     mode_low = plot_with_mode(losses, "Low Var (Quadrature)", "blue")
+#     mode_high = plot_with_mode(basic_losses, "High Var (Basic)", "orange")
+# except:
+#     plt.hist(losses, alpha=0.5, density=True, label="Low Var (Quadrature)", color="blue")
+#     plt.hist(basic_losses, alpha=0.5, density=True, label="High Var (Basic)", color="orange")
+# plt.legend()
+# plt.xlim((min(losses.min(), basic_losses.min()) - 2, max(np.quantile(losses, 0.95), np.quantile(basic_losses, 0.95)) + 2))
+# plt.savefig(base_path / "image.png")
 
 engine.quit()
