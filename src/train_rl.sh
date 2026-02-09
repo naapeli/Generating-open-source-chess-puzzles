0f7534f954708d67515d26f92754213d12e7e05e
Selkee Aatu <aatu.selkee@gmail.com>
2026-02-09 15:52:21 +0200

    RL distributed training code almost working (has some silent bug somewhere)

--- src/train_rl.sh
+++ src/train_rl.sh
@@ -1,12 +1,26 @@
 #!/bin/bash -l
+# #SBATCH --time=00:30:00
+# #SBATCH --output=output.out
+# #SBATCH --mem=64G
+# #SBATCH --gpus=1
+# #SBATCH --gres=min-vram:32g
+# #SBATCH --cpus-per-gpu=10
+
+
+# module load mamba
+# source activate environment
+# srun python src/train_rl.py
+
 #SBATCH --time=00:30:00
 #SBATCH --output=output.out
-#SBATCH --mem=64G
-#SBATCH --gpus=1
-#SBATCH --gres=min-vram:32g
-#SBATCH --cpus-per-gpu=10
+#SBATCH --mem=128G
+#SBATCH --nodes=1
+#SBATCH --ntasks=2
+# #SBATCH --gpus=h100:4
+#SBATCH --gpus=2
+#SBATCH --cpus-per-task=32
 
 
 module load mamba
 source activate environment
-srun python src/train_rl.py
+srun python src/train_rl.py --distributed
