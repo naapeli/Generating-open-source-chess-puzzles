e4534d77613e34c7311a7e8e65f696ba145c41bb
Selkee Aatu <aatu.selkee@gmail.com>
2026-02-10 08:57:52 +0200

    Started a larger RL run

--- src/train_rl.sh
+++ src/train_rl.sh
@@ -1,23 +1,10 @@
 #!/bin/bash -l
-# #SBATCH --time=00:30:00
-# #SBATCH --output=output.out
-# #SBATCH --mem=64G
-# #SBATCH --gpus=1
-# #SBATCH --gres=min-vram:32g
-# #SBATCH --cpus-per-gpu=10
-
-
-# module load mamba
-# source activate environment
-# srun python src/train_rl.py
-
-#SBATCH --time=00:30:00
+#SBATCH --time=24:00:00
 #SBATCH --output=output.out
 #SBATCH --mem=128G
 #SBATCH --nodes=1
-#SBATCH --ntasks=2
-# #SBATCH --gpus=h100:4
-#SBATCH --gpus=2
+#SBATCH --ntasks=4
+#SBATCH --gpus=h100:4
 #SBATCH --cpus-per-task=32
 
 
