360e18ec28ddf638b60ae752e1bdc50238395ead
Selkee Aatu <aatu.selkee@gmail.com>
2026-02-16 11:09:53 +0200

    Starting a second larger run

--- src/train_rl.sh
+++ src/train_rl.sh
@@ -1,10 +1,13 @@
 #!/bin/bash -l
-#SBATCH --time=01:00:00
+#SBATCH --time=36:00:00
 #SBATCH --output=output.out
-#SBATCH --mem=32G
+#SBATCH --mem=128G
 #SBATCH --nodes=1
 #SBATCH --ntasks=8
 #SBATCH --gpus=h200:8
+# #SBATCH --ntasks=4
+# #SBATCH --gpus=4
+# #SBATCH --gres=min-vram:32g
 #SBATCH --cpus-per-task=16
 
 
