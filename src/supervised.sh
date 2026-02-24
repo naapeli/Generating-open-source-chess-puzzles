9f728941f5f4bfb92003f0080c941596ea924f5c
Selkee Aatu <aatu.selkee@gmail.com>
2026-02-24 08:46:25 +0200

    Retrained the base model. Changed the conditioning of the denoising network to use cross attention instead of adding the embeddings.

--- src/supervised.sh
+++ src/supervised.sh
@@ -1,15 +1,21 @@
 #!/bin/bash -l
 #SBATCH --job-name=MaskedDiffusion
+# #SBATCH --time=02-00
 #SBATCH --time=12:00:00
 #SBATCH --output=output.out
 #SBATCH --mem=128G
 #SBATCH --nodes=1
+# #SBATCH --ntasks=8
+# #SBATCH --gpus=h200:8
 #SBATCH --ntasks=4
-#SBATCH --gpus=h100:4
+#SBATCH --gpus=a100:4
+# #SBATCH --ntasks=4
+# #SBATCH --gpus=4
+# #SBATCH --gres=min-vram:80g
 #SBATCH --cpus-per-gpu=10
 
 
 module load mamba
+module load triton/2024.1-gcc gcc/12.3.0  # needed for torch.compile
 source activate environment
-srun python src/supervised_training.py --distributed --continue_from_checkpoint
-# srun python src/supervised_training.py
+srun python src/supervised_training.py --distributed
