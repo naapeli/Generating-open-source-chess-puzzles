7d8e8281ce36031637c5bd8609186069e63f8953
Selkee Aatu <aatu.selkee@gmail.com>
2026-03-17 09:07:59 +0200

    Continued the good RL run

--- src/train_rl.sh
+++ src/train_rl.sh
@@ -19,4 +19,4 @@
 module load mamba
 module load triton/2024.1-gcc gcc/12.3.0  # needed for torch.compile
 source activate environment
-srun python src/train_rl.py  # --continue_from_checkpoint # --distributed
+srun python src/train_rl.py --continue_from_checkpoint # --distributed
