f9115f3aeeb5b719670fb3c5fc68b22ba2984601
Selkee Aatu <aatu.selkee@gmail.com>
2026-03-30 10:56:48 +0300

    started a RL hyperparameter tuning run

---
+++ src/tune_rl.sh
@@ -0,0 +1,17 @@
+#!/bin/bash -l
+#SBATCH --time=0-24:00:00
+#SBATCH --output=outputrl.out
+#SBATCH --mem=64G
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --gpus=h200:1
+# #SBATCH --partition=gpu-b300-288g-short
+# #SBATCH --gpus=1
+# #SBATCH --partition=gpu-debug
+#SBATCH --cpus-per-task=16
+
+
+module load mamba
+module load triton/2024.1-gcc gcc/12.3.0  # needed for torch.compile
+source activate environment
+srun python src/tune_rl.py --n_trials 10 --study_name espo_additive_reward
