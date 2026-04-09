26e40099dfff4a7a15cbd07fcc2274b2379326e6
naapeli <aatu.selkee@gmail.com>
2026-04-09 14:00:14 +0300

    One can now dynamically select if the model is able to output moves during position geneartion

--- src/generate_positions.sh
+++ src/generate_positions.sh
@@ -4,11 +4,12 @@
 #SBATCH --mem=64G
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
-#SBATCH --gpus=h200:1
+#SBATCH --gpus=1
+#SBATCH --constraint="a100|h100|h200|b300"
 #SBATCH --cpus-per-gpu=16
 
 
 module load mamba
 module load triton/2024.1-gcc gcc/12.3.0  # needed for torch.compile
 source activate environment
-srun python src/generate_positions.py
+srun python src/generate_positions.py  --model-type no-moves
