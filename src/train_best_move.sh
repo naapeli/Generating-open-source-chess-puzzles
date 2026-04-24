b1139bfa78a3cfbff364d38800020b921eeb56d6
Selkee Aatu <aatu.selkee@gmail.com>
2026-04-24 09:21:31 +0300

    Start experimenting with token-level RL

--- src/train_best_move.sh
+++ src/train_best_move.sh
@@ -4,14 +4,14 @@
 #SBATCH --mem=64G
 #SBATCH --nodes=1
 #SBATCH --account=ellis_users
-#SBATCH --constraint="h100|h200|b300"  # a100|
-#SBATCH --ntasks=2
-#SBATCH --gpus=2
+#SBATCH --constraint="h100|h200"  # a100|
+#SBATCH --ntasks=1
+#SBATCH --gpus=1
 #SBATCH --cpus-per-gpu=10
 
 
 module load mamba
 module load triton/2024.1-gcc gcc/12.3.0  # needed for torch.compile
 source activate environment
-# srun python src/train_best_move.py --distributed --run_name run1 --starting_model_path runs/supervised/no_context_move_model/model_0600000.pt
-srun python src/train_best_move.py --distributed --run_name run1 --starting_model_path runs/supervised/best_move_model/model_0980000.pt
+srun python src/train_best_move.py --distributed --run_name run2 --starting_model_path runs/supervised/no_context_move_model/model_0600000.pt
+# srun python src/train_best_move.py --distributed --run_name run2 --starting_model_path runs/supervised/best_move_model/model_0980000.pt
--- src/train_rl.sh
+++ src/train_rl.sh
@@ -1,11 +1,13 @@
 #!/bin/bash -l
-#SBATCH --time=0-48:00:00
+#SBATCH --time=0-24:00:00
+# #SBATCH --time=0-00:30:00
 #SBATCH --output=outputrl.out
 #SBATCH --mem=64G
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --gpus=1
-#SBATCH --constraint="h100|h200|b300"  # a100|
+#SBATCH --account=ellis_users
+#SBATCH --constraint="h100|h200"  # a100|
 # #SBATCH --partition=gpu-debug
 #SBATCH --cpus-per-task=16
 
@@ -13,4 +15,4 @@
 module load mamba
 module load triton/2024.1-gcc gcc/12.3.0  # needed for torch.compile
 source activate environment
-srun python src/train_rl.py --run_name run2 --reference_path "./src/runs/supervised/real_model_v2/model_0940000.pt" --lr 1e-6 --beta 1e-1 --group_size 4 --batch_size 8 --temperature 1e-2 --reward_structure "rewards = legal_position * pass_diversity_filtering * (unique_solution + counter_intuitive_solution)" --n_gradient_updates_per_generation 1 --save_period 500
+srun python src/train_rl.py --run_name test3 --reference_path "./src/runs/supervised/no_context_move_model/model_0600000.pt" --lr 1e-7 --beta 100 --group_size 16 --batch_size 2 --temperature 1 --reward_structure "rewards = torch.where(legal_position & unique_solution & counter_intuitive_solution, 1.0, 0.0); rewards = torch.where(legal_position, rewards, -2.0)" --n_gradient_updates_per_generation 1 --save_period 500 --weight_decay 0
